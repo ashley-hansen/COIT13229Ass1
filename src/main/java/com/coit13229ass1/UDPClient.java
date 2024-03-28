4dde24d5b550cd473fd6ce73143dd2bce3022a52
ashley-hansen <ashley.hansen@cqumail.com>
2024-03-28 23:47:18 +1000

    updated package name
    removed my name from package descriptor

--- src/main/java/com/coit13229ass1/UDPClient.java
+++ src/main/java/com/coit13229ass1/UDPClient.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.ashley.coit13229ass1;
+package com.coit13229ass1;
 
 /**
  *
