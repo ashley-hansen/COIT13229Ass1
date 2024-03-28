70b0b18526326c26b94f5f649680bb79e548b453
ashley-hansen <ashley.hansen@cqumail.com>
2024-03-28 23:51:59 +1000

    Update Member.java

--- src/main/java/com/coit13229ass1/Member.java
+++ src/main/java/com/coit13229ass1/Member.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.coit13229ass1;
 
-/**
- *
- * @author ashle
- */
+
 public class Member {
+    private String name;
+    private String adress;
+    private String phone;
+
+    public Member() {
+    }
+
+    public Member(String name, String adress, String phone) {
+        this.name = name;
+        this.adress = adress;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" + "name=" + name + ", adress=" + adress + ", phone=" + phone + '}';
+    }
+    
     
 }
