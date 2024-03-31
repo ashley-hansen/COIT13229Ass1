7b25b97df66cc84c7461c2155fb3c73a9e994e53
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-01 03:21:14 +1000

    dataEntry function added

--- src/main/java/com/coit13229ass1/TCPClient.java
+++ src/main/java/com/coit13229ass1/TCPClient.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.coit13229ass1;
 
-/**
- *
- * @author ashle
- */
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class TCPClient {
-    
-}
+
+    public static void main(String[] args) {
+
+    }// end of main method
+
+    public static void dataEntry() {
+        boolean isNext = true;
+
+        while (isNext) {
+
+            String name;
+            String address;
+            String phone;
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter name for member number:");
+            name = input.nextLine();
+            System.out.println("Enter address for member number:");
+            address = input.nextLine();
+            System.out.println("Enter phone for member number:");
+            phone = input.nextLine();
+            System.out.println("would you like to enter details for another member?: y or n?");
+            String cont = input.nextLine();
+            System.out.println(cont);
+
+            if (cont.equals("n")) {
+                break;
+            }
+
+        }
+    }
+}//end of class
