e89279c251465f91b281972a2a1e0a5f8c7464b8
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-01 03:22:22 +1000

    removed netbeans default comments

--- src/main/java/com/coit13229ass1/UDPServer.java
+++ src/main/java/com/coit13229ass1/UDPServer.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.coit13229ass1;
 
-/**
- *
- * @author ashle
- */
 public class UDPServer {
     
 }
