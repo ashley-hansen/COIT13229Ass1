3cf23d78b5ca08f53451b0df03d752a42bebd69e
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-03 00:58:15 +1000

    Update TCPServer.java

--- src/main/java/com/coit13229ass1/TCPServer.java
+++ src/main/java/com/coit13229ass1/TCPServer.java
@@ -6,43 +6,37 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class TCPServer {
-    public static void main(String[] args) throws FileNotFoundException{
-        
-        String name;
-        String address;
-        String phone;
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+
+    public static void readWrite(String name, String address, String phone) throws FileNotFoundException {
+
         ArrayList<Member> memberArray = new ArrayList<Member>();
         ArrayList<Member> newArray = new ArrayList<Member>();
-        
-        
-        
-        memberArray.add(new Member("Jerry Brown","123 Thorn st","123 456"));
-        memberArray.add(new Member("Reece the Beast","555 Egg st","123 987"));
-        memberArray.add(new Member("Ollie Flannagan","987 Rose Ln","456 487"));
-        
+        memberArray.add(new Member("Jerry Brown", "123 Thorn st", "123 456"));
+        memberArray.add(new Member("Reece the Beast", "555 Egg st", "123 987"));
+        memberArray.add(new Member("Ollie Flannagan", "987 Rose Ln", "456 487"));
+
         File memberFile = new File("memberlist.txt");
         PrintStream print = new PrintStream(memberFile);
         Scanner fileScanner = new Scanner(memberFile);
-        for(int i =0; i <= memberArray.size()-1; i++){
+        for (int i = 0; i <= memberArray.size() - 1; i++) {
             print.println(memberArray.get(i).getName());
             print.println(memberArray.get(i).getAdress());
             print.println(memberArray.get(i).getPhone());
         }
-        
-        while(fileScanner.hasNext()){
+
+        while (fileScanner.hasNext()) {
             name = fileScanner.nextLine();
             address = fileScanner.nextLine();
             phone = fileScanner.nextLine();
-            
-            newArray.add(new Member(name,address,phone));
-                       
+
+            newArray.add(new Member(name, address, phone));
+
         }
-        
-        System.out.println(newArray.get(1));
-        
-        
     }
-    
+
 }
