1eee6a31ff8ed0d77f8ee02515c6bff8be0d89f6
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-03 04:27:26 +1000

    updated

--- src/main/java/com/coit13229ass1/TCPServer.java
+++ src/main/java/com/coit13229ass1/TCPServer.java
@@ -9,21 +9,19 @@ public class TCPServer {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
-        try{
-			int serverPort = 8000; 
-			ServerSocket listenSocket = new ServerSocket(serverPort);
-			
-			while(true) {
-				Socket clientSocket = listenSocket.accept();
-				Connection c = new Connection(clientSocket);
-			}
+        try {
+            int serverPort = 8000;
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+
+            while (true) {
+                Socket clientSocket = listenSocket.accept();
+                Connection c = new Connection(clientSocket);
+            }
 
         } catch (IOException e) {
 
         }
     }//End Main method
-
-    
 
 }//End of class
 
@@ -39,28 +37,45 @@ class Connection extends Thread {
             clientSocket = aClientSocket;
             input = new ObjectInputStream(clientSocket.getInputStream());
             output = new ObjectOutputStream(clientSocket.getOutputStream());
-            System.out.println("Step 2");
+
             this.start();
         } catch (IOException e) {
             System.out.println("Connection:" + e.getMessage());
         }
-        
-        
-        }
-    public void run(){
-            try{
-               System.out.println("Step 3");
-               Member member1 = (Member)input.readObject();
-               System.out.println(member1);
-               output.writeObject(member1);
-               
-             
-             
-            }catch(IOException e){
-                System.out.println("IOException" + e.getMessage());
-            }catch(ClassNotFoundException e){
-                System.out.println("Class not found exception" + e.getMessage());
-            }
-          
+
     }
+
+    public void run() {
+
+        boolean running = true;
+        String name;
+        String address;
+        String phone;
+
+        try {
+            File memberFile = new File("memberlist.txt");
+            PrintStream writer = new PrintStream(memberFile);
+            Member member1 = (Member) input.readObject();
+
+            name = member1.getName();
+            writer.println(name);
+            address = member1.getAdress();
+            writer.println(address);
+            phone = member1.getPhone();
+            writer.println(phone);
+            ArrayList<Member> memberList = new ArrayList<Member>();
+
+            memberList.add(new Member(name, address, phone));
+            for (int i = 0; i < memberList.size(); i++) {
+                System.out.println(memberList.size());
+            }
+
+//            output.writeObject(member1);
+        } catch (IOException e) {
+            System.out.println("IOException" + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found exception" + e.getMessage());
+        }
+    }
+
 }
