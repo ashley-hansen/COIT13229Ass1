9d523ae5d940d8b1626932d3e72868aa2abf1ff6
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-03 05:07:14 +1000

    Update TCPServer.java

--- src/main/java/com/coit13229ass1/TCPServer.java
+++ src/main/java/com/coit13229ass1/TCPServer.java
@@ -2,12 +2,32 @@ package com.coit13229ass1;
 
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class TCPServer {
 
     public static void main(String[] args) {
-
+           Socket socket = null;
+           InputStreamReader input = null;
+           OutputStreamWriter output = null;
+           BufferedReader br = null;
+           BufferedWriter bw = null;
+           ServerSocket sSocket = null;
+           
+        try {
+            sSocket = new ServerSocket(8000);
+            
+            while(true){
+             socket = sSocket.accept();
+             
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+           
+           
+           
     }
 }
