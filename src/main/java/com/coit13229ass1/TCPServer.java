e75ed43386d8bc777758366fc94ab66851413c63
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-03 02:39:26 +1000

    Update TCPServer.java

--- src/main/java/com/coit13229ass1/TCPServer.java
+++ src/main/java/com/coit13229ass1/TCPServer.java
@@ -1,42 +1,66 @@
 package com.coit13229ass1;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TCPServer {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
-    }
+        try{
+			int serverPort = 8000; 
+			ServerSocket listenSocket = new ServerSocket(serverPort);
+			
+			while(true) {
+				Socket clientSocket = listenSocket.accept();
+				Connection c = new Connection(clientSocket);
+			}
 
-    public static void readWrite(String name, String address, String phone) throws FileNotFoundException {
-
-        ArrayList<Member> memberArray = new ArrayList<Member>();
-        ArrayList<Member> newArray = new ArrayList<Member>();
-        memberArray.add(new Member("Jerry Brown", "123 Thorn st", "123 456"));
-        memberArray.add(new Member("Reece the Beast", "555 Egg st", "123 987"));
-        memberArray.add(new Member("Ollie Flannagan", "987 Rose Ln", "456 487"));
-
-        File memberFile = new File("memberlist.txt");
-        PrintStream print = new PrintStream(memberFile);
-        Scanner fileScanner = new Scanner(memberFile);
-        for (int i = 0; i <= memberArray.size() - 1; i++) {
-            print.println(memberArray.get(i).getName());
-            print.println(memberArray.get(i).getAdress());
-            print.println(memberArray.get(i).getPhone());
-        }
-
-        while (fileScanner.hasNext()) {
-            name = fileScanner.nextLine();
-            address = fileScanner.nextLine();
-            phone = fileScanner.nextLine();
-
-            newArray.add(new Member(name, address, phone));
+        } catch (IOException e) {
 
         }
-    }
+    }//End Main method
 
+    
+
+}//End of class
+
+class Connection extends Thread {
+
+    ObjectInputStream input;
+    ObjectOutputStream output;
+    Socket clientSocket;
+
+    public Connection(Socket aClientSocket) {
+
+        try {
+            clientSocket = aClientSocket;
+            input = new ObjectInputStream(clientSocket.getInputStream());
+            output = new ObjectOutputStream(clientSocket.getOutputStream());
+            System.out.println("Step 2");
+            this.start();
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+        
+        
+        }
+    public void run(){
+            try{
+               System.out.println("Step 3");
+               Member member1 = (Member)input.readObject();
+               System.out.println(member1);
+               output.writeObject(member1);
+               
+             
+             
+            }catch(IOException e){
+                System.out.println("IOException" + e.getMessage());
+            }catch(ClassNotFoundException e){
+                System.out.println("Class not found exception" + e.getMessage());
+            }
+          
+    }
 }
