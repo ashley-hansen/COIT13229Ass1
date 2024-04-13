df4db513ade327cb792205d0b01556f384aceff4
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-14 01:28:50 +1000

    Update UDPServer.java

--- src/main/java/com/coit13229ass1/UDPServer.java
+++ src/main/java/com/coit13229ass1/UDPServer.java
@@ -8,47 +8,55 @@ import java.util.Scanner;
 public class UDPServer {
 
     public static void main(String[] args) throws SocketException, IOException, ClassNotFoundException {
-
+        //initialise variable to creak socket connection
         int port = 2276;
         DatagramSocket serverSocket = new DatagramSocket(port);
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Member> memberList = new ArrayList<Member>();
+        ArrayList<Member> memberList = new ArrayList<Member>();// arraylist for object deserialisation
+        //while loop to keep connection open
         while (true) {
             byte[] recieveBuffer = new byte[1024];
             byte[] sendBuffer = new byte[1024];
 
+            //create datagram object to recieve message
             DatagramPacket recievePacket = new DatagramPacket(recieveBuffer, recieveBuffer.length);
             serverSocket.receive(recievePacket);
             InetAddress address = recievePacket.getAddress();
             int portNum = recievePacket.getPort();
 
+            //create string onject from client to bytestream and perform action
+            //empty string response and server will return the list.
             String clientMessage = new String(recievePacket.getData());
-            System.out.println("\nCLient: " + clientMessage);
-            System.out.println("\nServer :");
+            System.out.println("\nCLient response recieved " + clientMessage);
+            System.out.println("\nServer : performing required action");
 
+            //create object from serialised memberlistObject file
             FileInputStream sIn = new FileInputStream("memberListObject");
             ObjectInputStream sOut = new ObjectInputStream(sIn);
             memberList = (ArrayList) sOut.readObject();
+
+            //build stringbuilder object a newline string
             StringBuilder sb = new StringBuilder();
             String newLine = "\n";
 
+            //create string for message
             for (int i = 0; i < memberList.size(); i++) {
                 sb.append(memberList.get(i) + newLine);
             }
             String str = sb.toString();
 
-            System.out.println(memberList);
-
+            //create bytestream to send list back to server with outgoing datagram object
             sendBuffer = str.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, address, portNum);
-            serverSocket.send(sendPacket);
+            serverSocket.send(sendPacket);//send the list message
+            //clost if message recieved is equal to bye
             if (clientMessage.equalsIgnoreCase("bye")) {
                 System.out.println("Connection ended by server");
                 break;
             }
         }
 
-        serverSocket.close();
+        serverSocket.close();//close socket connection
 
     }
 }
