a5212571fb91f30d5475ef3563666152ce0f7531
ashley-hansen <ashley.hansen@cqumail.com>
2024-04-13 23:35:49 +1000

    2 way UDP connection working

--- src/main/java/com/coit13229ass1/UDPClient.java
+++ src/main/java/com/coit13229ass1/UDPClient.java
@@ -1,6 +1,39 @@
 package com.coit13229ass1;
 
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class UDPClient {
+        public static void main(String[] args) throws IOException, SocketException {
+        int port = 2276;
+        Scanner scanner = new Scanner(System.in);
+        InetAddress address = InetAddress.getByName("localhost");
+        DatagramSocket clientSocket = new DatagramSocket();
+
+        while(true){
+
+            byte[] sendBuffer = new byte[1024];
+            byte[] recieveBuffer = new byte[1024];
+
+            System.out.println("\nClient: ");
+            String clientMessage = scanner.nextLine();
+            sendBuffer = clientMessage.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer,sendBuffer.length,address, port);
+            clientSocket.send(sendPacket);
+
+            if(clientMessage.equalsIgnoreCase("bye")){
+                System.out.println("Connection ended by client");
+                break;
+            }
+
+            DatagramPacket receivePacket = new DatagramPacket(recieveBuffer,recieveBuffer.length);
+            clientSocket.receive(receivePacket);
+            String serverMessage = new String(receivePacket.getData());
+            System.out.println("\nServer: "+serverMessage);
+        }
+        clientSocket.close();
+    }
     
 }
--- src/main/java/com/coit13229ass1/UDPServer.java
+++ src/main/java/com/coit13229ass1/UDPServer.java
@@ -1,5 +1,54 @@
 package com.coit13229ass1;
 
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UDPServer {
-    
+        public static void main(String[] args) throws SocketException, IOException {
+
+        int port = 2276;
+        DatagramSocket serverSocket = new DatagramSocket(port);
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            byte[] recieveBuffer = new byte[1024];
+            byte[] sendBuffer = new byte[1024];
+
+            DatagramPacket recievePacket = new DatagramPacket(recieveBuffer,recieveBuffer.length);
+            serverSocket.receive(recievePacket);
+            InetAddress address = recievePacket.getAddress();
+            int portNum = recievePacket.getPort();
+
+            String clientMessage = new String(recievePacket.getData());
+            System.out.println("\nCLient: "+clientMessage);
+            System.out.println("\nServer :");
+            String serverMessage = scanner.nextLine();
+            sendBuffer = serverMessage.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer,sendBuffer.length, address,portNum);
+            serverSocket.send(sendPacket);
+            if(serverMessage.equalsIgnoreCase("bye")){
+                System.out.println("Connection ended by server");
+                break;
+            }
+        }
+
+        serverSocket.close();
+
+    }
 }
+
+
+
+//        ArrayList<Member> memberList = new ArrayList<Member>();
+//        
+//        try{
+//        FileInputStream sIn = new FileInputStream("memberListObject");
+//        ObjectInputStream sOut = new ObjectInputStream(sIn);
+//        
+//        memberList = (ArrayList) sOut.readObject();
+//        
+//        System.out.println(memberList);
+//        }catch(Exception e){
+//        }
